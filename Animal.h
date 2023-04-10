7c4e3cd4a8bb7675a3261cc1a9351ab450eea209
Sam Ahmadizadeh <sam@Sams-MacBook-Pro-2.local>
2023-04-10 14:17:33 +0330

    making animal.h look great

---
+++ Animal.h
@@ -0,0 +1,18 @@
+#ifndef AP_MIDTERM_PROJECT_ANIMAL_H
+#define AP_MIDTERM_PROJECT_ANIMAL_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Cell.h"
+
+class Animal : Cell{
+
+float tashaboh_jenetici(Animal);
+
+
+};
+
+
+
+
+#endif //AP_MIDTERM_PROJECT_CELL_H
