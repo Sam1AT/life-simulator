f624f8ea108a6e599ee58b7ad2b0ed931f8996fe
Sam1AT <alileon32@gmail.com>
2023-04-13 10:21:11 +0330

    start of making menu

--- main.cpp
+++ main.cpp
@@ -4,24 +4,70 @@
 #include "Animal.cpp"
 #include <math.h>
 
+using namespace std;
 
 
 
 int main(){
-    Cell k;
-    //k.add(1);
-   // k.jahesh_K_DNA("T","A",2,0);
-    Animal sam , ali;
-    sam.c.add(2);
-    ali.c.add(2);
-    string s1 = "GCATGCU";
-    string s2 = "GATTACA";
+    int option = 10;
+    cout << "--------------------------------------------------------------------------------";
+    cout << "|                       Welcome to the class of life app                       |" << endl;
+    cout << "|                                 Authors :                                    |" << endl;
+    cout << "|                           Sam Ahmadizadeh Tourzani                           |" << endl;
+    cout << "|                           Elnaz                                              |" << endl;
+    cout << "--------------------------------------------------------------------------------";
 
-    //int percentage = sam.simularity(ali);
-    //cout <<"main runed 1 "<< endl;
-    //cout << "Score: " << (sam == ali);
-    //Animal s = ali + sam;
-    sam.death();
-    return 0;
+while(option != 0)
+{
+    cout<<"Please choose one of the options below : "<<endl;
+    cout << "1) Making a new animal." << endl;
+    cout << "2) Adding Chromosome to an Animal." << endl;
+    cout << "3) Making DNA from a RNA." << endl;
+    cout << "4) Small mutation." << endl;
+    cout << "5) Big mutation." << endl;
+    cout << "6) Reversed mutation." << endl;
+    cout << "7) Finding all the palindroms of a Chromosome." << endl;
+    cout << "*** For exit please enter 0 ***" << endl;
+    cin >> option;
+    if(option == 1)
+    {
+
+    }
+    else if(option == 2)
+    {
+
+    }
+    else if(option == 3)
+    {
+
+    }
+    else if(option == 4)
+    {
+
+    }
+    else if(option == 5)
+    {
+
+    }
+    else if(option == 6)
+    {
+
+    }
+    else if(option == 7)
+    {
+
+    }
+    else if(option != 0)
+    {
+        cout << "! ! !    Invalid option please choose a valid one   ! ! !" << endl;
+    }
+}
+
+
+cout << "Thanks for using our app =)";
+
+
+
+
 }
 
